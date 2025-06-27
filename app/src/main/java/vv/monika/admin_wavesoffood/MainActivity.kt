b3add50326b762f399254ee280d25062d8c15d19
MONIKAvv <monikalonewolf35@gmail.com>
2025-06-27 15:41:16 +0530

    admin setup

---
+++ app/src/main/java/vv/monika/admin_wavesoffood/MainActivity.kt
@@ -0,0 +1,26 @@
+package vv.monika.admin_wavesoffood
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import vv.monika.admin_wavesoffood.ui.theme.Admin_WavesofFoodTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+       setContentView(R.layout.activity_main)
+//        start code from here
+
+    }
+}
+
+
---
+++ app/src/main/java/vv/monika/admin_wavesoffood/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package vv.monika.admin_wavesoffood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
